d3846b9aedd0c6d317b4dd9bbdd7c13376e86896
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-09-04 22:36:32 +0900

    Update dc.py

--- visualization/dc.py
+++ visualization/dc.py
@@ -12,3 +12,31 @@ ax.plot(austin_weather["MONTH"],austin_weather["MLY-PRCP-NORMAL"])
 
 # Call the show function
 plt.show()
+
+# Plot Seattle data, setting data appearance
+ax.plot(seattle_weather["MONTH"], 
+    seattle_weather["MLY-PRCP-NORMAL"], 
+    color = "b", marker = "o")
+
+# Plot Austin data, setting data appearance
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], 
+    color = "r", marker = "v")
+
+# Call show to display the resulting plot
+plt.show()
+
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"])
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"])
+
+# Customize the x-axis label
+ax.set_xlabel("Time(months)")
+
+# Customize the y-axis label
+ax.set_ylabel("Precipitation (inches)")
+
+# Add the title
+ax.set_title("Weather patterns in Austin and Seattle")
+
+# Display the figure
+plt.show()
+
